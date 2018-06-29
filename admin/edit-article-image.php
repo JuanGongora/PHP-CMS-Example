83a7957606978ed69f46f1b2a10674e23d4eb4f7
JuanGongora <gongora.animations@gmail.com>
2018-06-29 18:25:05 -0400

    implementing file type checking for uploading

--- admin/edit-article-image.php
+++ admin/edit-article-image.php
@@ -39,13 +39,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 break;
 
             case UPLOAD_ERR_INI_SIZE:
-                throw new Exception("File is too large, please make sure it is 2Mb");
+                throw new Exception("File is too large to upload");
                 break;
 
             default:
 
                 throw new Exception("An error occurred");
         }
+
+        if ($_FILES["file"]["size"] > 1000000) {
+            throw new Exception("File is too large");
+        }
+
+        //array of acceptable MIME types for the site
+        $mime_types = ["image/gif", "image/png", "image/jpeg"];
+
+        //create a new fileinfo resource and return the mime type
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+
+        //$finfo resource returned by finfo_open(), with the second parameter in
+        //finfo_file() being the name of a file to be checked by the HTTP File Upload variables
+        $mime_type = finfo_file($finfo, $_FILES["file"]["tmp_name"]);
+        //files uploaded are temporarily saved at: $_FILES["file"]["tmp_name"] of array
+
+        //$mime_type returns a string, so checking if the type is in array
+        //we replace the type in the $mime_types array, with that $mime_type value
+        if (!in_array($mime_type, $mime_types)) {
+            throw new Exception("Invalid file type");
+        }
+
     } catch (Exception $e) {
         //the catch outputs the errors for us after they were thrown above
         echo $e->getMessage();
