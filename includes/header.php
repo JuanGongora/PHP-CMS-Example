cede5678e0c974addd19a7593249a79715ac7d6e
JuanGongora <gongora.animations@gmail.com>
2018-07-03 17:03:12 -0400

    implementing Bootstrap into CMS design

--- includes/header.php
+++ includes/header.php
@@ -2,21 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+          integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+          crossorigin="anonymous">
     <title>Blog Site</title>
 </head>
     <body>
 
+        <div class="container">
+
         <nav>
-            <ul>
-                <li><a href="/">Home</a> </li>
+            <ul class="nav">
+                <li class="nav-item"><a class="nav-link" href="/">Home</a> </li>
                 <?php if (Auth::isLoggedIn()): ?>
 
-                    <li><a href="/admin/">Admin</a></li>
-                    <li><a href="/logout.php">Log out</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/admin/">Admin</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/logout.php">Log out</a></li>
 
                 <?php else: ?>
 
-                <li><a href="/login.php">Log in</a></li>
+                <li class="nav-item"><a class="nav-link" href="/login.php">Log in</a></li>
 
                 <?php endif; ?>
             </ul>
